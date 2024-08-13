b77ccc68eb02072cefc9d130f561176f18eea026
Drex72 <femidavooo@gmail.com>
2024-08-13 12:00:58 +0100

    added the animated logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-import React from 'react';
-import Image from 'next/image';
-import { HeaderProps } from './models';
-import styles from './header.module.scss';
 import { classNames } from '@/utils/classNames';
+import React from 'react';
+// import AnimatedLogo from '../../../public/animated-devfest-logo.svg';
+import styles from './header.module.scss';
+import { HeaderProps } from './models';
+import Image from 'next/image';
 
 const Header: React.FC<HeaderProps> = ({ navContent, className, handleClick }) => {
   return (
     <header className={classNames(styles.header, className)}>
       <span>
         <Image
-          src='/icons/devfest-logo.svg'
+          src='/animated-devfest-logo.svg'
           alt='DevFest Lagos logo'
           width={118}
           height={34}
           priority={true}
         />
+        {/* <AnimatedLogo /> */}
       </span>
       {navContent && (
         <nav>
